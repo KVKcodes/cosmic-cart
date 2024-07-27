51ead4e1c175fb0f225d83676a2a7f49f6ab9719
KVK Siddartha <sidduultralegend@gmail.com>
2024-07-28 01:05:43 +0530

    ues

--- src/main/java/material/management/Utils/DBUtil.java
+++ src/main/java/material/management/Utils/DBUtil.java
@@ -3,27 +3,13 @@ package material.management.Utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
-
-import io.github.cdimascio.dotenv.Dotenv;
 
 public class DBUtil {
-    private static final Dotenv dotenv = Dotenv.load();
-
-    private static final String URL = dotenv.get("DB_URL");
-    private static final String USER = dotenv.get("DB_USER");
-    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
-    private static final String SSL_CA = "./ca.pem";
+    private static final String URL = "jdbc:mysql://localhost:3306/cosmicDB";
+    private static final String USER = "sample_user";
+    private static final String PASSWORD = "sample4ever";
 
     public static Connection getConnection() throws SQLException {
-        Properties props = new Properties();
-        props.setProperty("user", USER);
-        props.setProperty("password", PASSWORD);
-        props.setProperty("useSSL", "true");
-        props.setProperty("requireSSL", "true");
-        props.setProperty("verifyServerCertificate", "true");
-        props.setProperty("trustCertificateKeyStoreUrl", "file:" + SSL_CA);
-
-        return DriverManager.getConnection(URL, props);
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
